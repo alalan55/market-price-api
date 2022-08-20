655db0ca392d13c2ceb692bba426d3327de4a4e2
aoliveira <aoliveira@keener.io>
2022-08-19 21:18:31 -0300

    Insere task service para criar tarefa

--- services/taskService.py
+++ services/taskService.py
@@ -1,12 +1,20 @@
 from sqlalchemy.orm import Session
-from models.model import Task
-from ..schemas.schemas import TaskSchema
+from db.models.task_model import Task
+from db.schemas import TaskSchema
 
 
-def get_tasks(db: Session, task: TaskSchema, skip:int=0, limit:int=100):
+def get_tasks(db: Session, skip: int = 0, limit: int = 100):
     return db.query(Task).offset(skip).limit(limit).all()
     # _user = User(name=user.name)
     # db.add(_user)
     # db.commit()
     # db.refresh(_user)
     # return _user
+
+
+def create_task(db: Session, task: TaskSchema):
+    _task = Task(title=task.title, quantity=task.quantity, value=task.value)
+    db.add(_task)
+    db.commit()
+    db.refresh(_task)
+    return _task
