0707c57b5e432b7eb20f520a9d2f8c2db9c0ad61
alalan55 <alla.ao4975@gmail.com>
2023-04-15 13:32:29 -0300

    Insere login

--- routes/auth.py
+++ routes/auth.py
@@ -1,9 +1,11 @@
-from fastapi import APIRouter, HTTPException, Depends
+from fastapi import APIRouter, HTTPException, Depends, status
 from passlib.context import CryptContext
 from database import SessionLocal
 from sqlalchemy.orm import Session
 from pydantic import BaseModel
 from typing import Optional
+from jose import jwt, JWTError
+from datetime import timedelta, datetime
 import models
 
 
@@ -24,6 +26,11 @@ class CreateUser(BaseModel):
     profile_pic: Optional[str]
 
 
+class UserLogin(BaseModel):
+    email: str
+    password: str
+
+
 def get_db():
     try:
         db = SessionLocal()
@@ -32,8 +39,34 @@ def get_db():
         db.close()
 
 
+def create_access_token(email: str, id: int, expires_delta: Optional[timedelta] = None):
+    encode = {"sub": email, "id": id}
+
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    encode.update({"exp": expire})
+
+    return jwt.encode(encode, SECRET_KEY, algorithm=ALGOTIGHTM)
+
+
 def password_hash(password):
     return bcrypt_context.hash(password)
+
+
+def verify_password(password, hashed):
+    return bcrypt_context.verify(password, hashed)
+
+
+def authenticate_user(email: str, password: str, db):
+    user = db.query(models.Users).filter(models.Users.email == email).first()
+
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
 
 
 @router.post("/user/create")
@@ -47,3 +80,33 @@ async def create_user(user: CreateUser, db: Session = Depends(get_db)):
     db.commit()
 
     return {'message': 'Usuário criado com sucesso!'}
+
+
+@router.post("/user/login")
+async def login(user: UserLogin, db: Session = Depends(get_db)):
+    user = authenticate_user(user.email, user.password, db)
+
+    if not user:
+        raise token_exception()
+
+    token_expires = timedelta(minutes=60)
+    token = create_access_token(
+        user.email, user.id, expires_delta=token_expires)
+
+    user.hashed_password = None
+    return successful_response(200, token, user)
+
+
+def token_exception():
+    token_exception_resp = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                                         detail="Usuário ou senha icorretos", headers={"WWW-Authenticate": "Bearer"})
+    return token_exception_resp
+
+
+def successful_response(status_code: int, token: Optional[str] = None, content: Optional[dict or list] = None):
+    return {
+        "status": status_code,
+        "message": "Sucesso!",
+        "content": content,
+        "token": token
+    }
