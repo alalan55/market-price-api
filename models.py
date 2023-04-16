57ffc6a1ab34f9bb6935c74654c5a0106fbebdbd
alalan55 <alla.ao4975@gmail.com>
2023-04-16 17:22:05 -0300

    Insere rota para criação de produto

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey, Float
 from sqlalchemy.orm import relationship
 from database import Base
 
@@ -20,7 +20,7 @@ class Products(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String)
-    price = Column(String)
+    price = Column(Float)
     buy_date = Column(String)
     buy_month = Column(Integer)
     buy_year = Column(Integer)
--- routes/product.py
+++ routes/product.py
@@ -8,7 +8,18 @@ from dtos.responses_dto import success_response_model
 import models
 
 
-router = APIRouter(prefix="/product")
+router = APIRouter(prefix="/product", tags=["Products"])
+
+
+class ProductCreate(BaseModel):
+    name: str
+    price: float
+    buy_date: str
+    buy_month: str
+    buy_year: str
+    quantity: int
+    type: int
+    owner_id: int
 
 
 def get_db():
@@ -28,3 +39,24 @@ async def get_all_products(db: Session = Depends(get_db), user: dict = Depends(g
         models.Products.owner_id == user.get("id")).all()
 
     return success_response_model(200, product_model)
+
+
+@router.post("/create")
+async def create_product(product: ProductCreate, db: Session = Depends(get_db), user: dict = Depends(get_current_user)):
+    if user is None:
+        raise get_user_exception()
+
+    product_model = models.Products()
+    product_model.name = product.name
+    product_model.price = product.price
+    product_model.buy_date = product.buy_date
+    product_model.buy_month = product.buy_month
+    product_model.buy_year = product.buy_year
+    product_model.quantity = product.quantity
+    product_model.type = product.type
+    product_model.owner_id = user.get("id")
+
+    db.add(product_model)
+    db.commit()
+
+    return success_response_model(201, product, "Produto criado com sucesso!")
