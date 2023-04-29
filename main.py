6cd9a5ad0f71e4a4b828551042102f061469095d
alalan55 <alla.ao4975@gmail.com>
2023-04-29 15:50:29 -0300

    Insere middleware

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from database import engine
 from routes import auth, user, product
+from fastapi.middleware.cors import CORSMiddleware
 import models
 
 app = FastAPI()
@@ -10,6 +11,14 @@ models.Base.metadata.create_all(bind=engine)
 app.include_router(auth.router)
 app.include_router(user.router)
 app.include_router(product.router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 
 @app.get("/")
